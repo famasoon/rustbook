f6882d8fbdea377aca85d8d2d788ae66dd4a4697
FAMASoon <famasoon@gmail.com>
2022-03-08 22:19:31 +0900

    Change to able reading  files

--- greplite/src/main.rs
+++ greplite/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use regex::Regex;
 use clap::{App, Arg};
 
@@ -5,15 +8,19 @@ fn main() {
     let args = App::new("grep-lite")
     .version("0.1")
     .about("searches for patterns")
-    .arg(Arg::with_name("pattern").help("The pattern to search for").takes_value(true).required(true)).get_matches();
+    .arg(Arg::with_name("pattern").help("The pattern to search for").takes_value(true).required(true))
+    .arg(Arg::with_name("input").help("The pattern to search for").takes_value(true).required(true)).get_matches();
+
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
-    let quote = "Every face, every shop, bedroom window, public-house, and
-    dark square is a picture feverishly turned--in search of what?
-    It is the same with books. What do we seek through millions of pages?";
 
-    for line in quote.lines() {
-        match re.find(line) {
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
