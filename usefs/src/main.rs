586a5783ab6bbf253ff805baf57994185be9dead
FAMASoon <famasoon@gmail.com>
2022-03-08 22:10:27 +0900

    Use BufReader.lines()

--- usefs/src/main.rs
+++ usefs/src/main.rs
@@ -4,18 +4,10 @@ use std::io::prelude::*;
 
 fn main() {
     let f = File::open("readme.md").unwrap();
-    let mut reader = BufReader::new(f);
+    let reader = BufReader::new(f);
 
-    let mut line = String::new();
-
-    loop {
-        let len = reader.read_line(&mut line).unwrap();
-        if len == 0 {
-            break;
-        }
-
-        println!("{} ({} bytes long)", line, len);
-
-        line.truncate(0);
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        println!("{} ({} bytes)", line, line.len());
     }
 }
