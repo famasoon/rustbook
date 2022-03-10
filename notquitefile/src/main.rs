0e59710eae84cc4ad692de0e80a330b5c94cd6ac
FAMASoon <famasoon@gmail.com>
2022-03-10 21:43:32 +0900

    Change to use new method

--- notquitefile/src/main.rs
+++ notquitefile/src/main.rs
@@ -6,6 +6,29 @@ struct File{
     data: Vec<u8>,
 }
 
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+}
+
 fn open(f: &mut File) -> bool {
     true
 }
@@ -14,25 +37,14 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-    read_length
-}
-
 fn main() {
-    let mut f1 = File{
-        name: String::from("f1.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let f1_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f1 = File::new_with_data("f1.txt", &f1_data);
 
     let mut buffer: Vec<u8> = vec![];
 
     open(&mut f1);
-    let f1_length = read(&f1, &mut buffer);
+    let f1_length = f1.read(&mut buffer);
     close(&mut f1);
 
     let text = String::from_utf8_lossy(&buffer);
