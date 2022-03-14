a62cc546191e76366ab39a2bfe07216893a12382
FAMASoon <famasoon@gmail.com>
2022-03-14 23:34:04 +0900

    Add trait sample

--- filestatus/src/main.rs
+++ filestatus/src/main.rs
@@ -1,3 +1,8 @@
+#![allow(dead_code)]
+
+use std::fmt;
+use std::fmt::{Display};
+
 #[derive(Debug,PartialEq)]
 enum FileState {
     Open,
@@ -9,6 +14,21 @@ struct File {
     name: String,
     data: Vec<u8>,
     state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{}{}>", self.name, self.state)
+    }
 }
 
 impl File {
@@ -60,4 +80,7 @@ fn main() {
     println!("{:?}", f);
     println!("{} is {} bytes long", &f.name, f_length);
     println!("{}", text);
+
+    println!("{:?}", f);
+    println!("{}", f);
 }
