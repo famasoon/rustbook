27a4dc6fb972aae7c6087b4c07bfc658db63c994
FAMASoon <famasoon@gmail.com>
2022-04-15 22:16:17 +0900

    Modify pointer-intro

--- memory/pointer-intro.rs
+++ memory/pointer-intro.rs
@@ -1,10 +1,40 @@
+use std::mem::size_of;
+
 static B: [u8; 10] = [99, 97, 114, 114, 121, 116, 111, 119, 101, 108];
 static C: [u8; 11] = [116, 104, 97, 110, 107, 115, 102, 105, 115, 104, 0];
 
 fn main() {
-    let a = 42;
-    let b = &B;
-    let c = &C;
+    let a: usize = 42;
+    let b: &[u8; 10] = &B;
+    let c: Box<[u8]> = Box::new(C);
 
-    println!("a: {}, b: {:p}, c: {:p}", a, b, c);
+    println!("a (符号のない整数):");
+    println!(" 場所: {:p}", &a);
+    println!(" サイズ: {:?} バイト", size_of::<usize>());
+    println!(" 参照先: {:?}", a);
+    println!();
+
+    println!("b (Bへの参照):");
+    println!(" 場所: {:p}", &b);
+    println!(" サイズ: {:?} バイト", size_of::<&[u8; 10]>());
+    println!(" 参照先: {:?}", b);
+    println!();
+
+    println!("c (Cを入れたボックス):");
+    println!(" 場所: {:p}", &c);
+    println!(" サイズ: {:?} バイト", size_of::<Box<[u8]>>());
+    println!(" 参照先: {:?}", c);
+    println!();
+
+    println!("B (10バイトの配列):");
+    println!(" 場所: {:p}", &B);
+    println!(" サイズ: {:?} バイト", size_of::<&[u8; 10]>());
+    println!(" 参照先: {:?}", B);
+    println!();
+
+    println!("C (11バイトの配列):");
+    println!(" 場所: {:p}", &C);
+    println!(" サイズ: {:?} バイト", size_of::<&[u8; 11]>());
+    println!(" 参照先: {:?}", C);
+    println!();
 }
